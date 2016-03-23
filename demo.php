289c693ccec34972e451e2bd7dd2a7fd6c2daf86
Derwin McGeary <derwinmcgeary+web@gmail.com>
2016-03-23 09:44:09 +0000

    use table and column names specificied in HW assignment

--- demo.php
+++ demo.php
@@ -9,7 +9,7 @@ $db_name="names";
 $link = mysql_connect($db_host, $db_username, $db_password);
 mysql_select_db($db_name, $link);
 
-$sql = "INSERT INTO names (c) VALUES ('$var')" ;
+$sql = "INSERT INTO user (name) VALUES ('$var')" ;
 $result = mysql_query($sql , $link);
 header ("Location: /list-names.php");
 
--- list-names.php
+++ list-names.php
@@ -6,7 +6,7 @@ $db_name="names";
 $link = mysql_connect($db_host, $db_username, $db_password);
 mysql_select_db($db_name, $link);
 
-$sql = "SELECT c FROM names" ;
+$sql = "SELECT name FROM user";
 $result = mysql_query($sql , $link);
 ?>
 <ul>
@@ -16,7 +16,7 @@ while($row = mysql_fetch_array($result))
 {
 ?>
 <li>
-<?php echo $row['c']; ?>
+<?php echo $row['name']; ?>
 </li>
 <?php
 }
