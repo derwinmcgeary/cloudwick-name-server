1d5260035e9eb399ab44a7ff043c1fb28c0d7379
Derwin McGeary <derwinmcgeary+web@gmail.com>
2016-03-24 12:33:52 +0000

    Protect against XSS

--- demo.php
+++ demo.php
@@ -1,7 +1,7 @@
 <?php
 
 $var = $_POST['input1'];
-
+$var2 = strip_tags($var);
 $db_username="httpd";
 $db_password="aspirine2710";
 $db_host="localhost";
@@ -9,7 +9,7 @@ $db_name="cloudwick";
 $link = mysql_connect($db_host, $db_username, $db_password);
 mysql_select_db($db_name, $link);
 
-$sql = "INSERT INTO user (name) VALUES ('$var')" ;
+$sql = "INSERT INTO user (name) VALUES ('$var2')" ;
 $result = mysql_query($sql , $link);
 header ("Location: /list-names");
 
